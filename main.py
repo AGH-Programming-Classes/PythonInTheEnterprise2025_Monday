f76348fe7f1f7b9289a1f6bf2f2605e47e824785
catsnravens <wik.ant.s@gmail.com>
2025-10-13 14:41:42 +0200

    centred window

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from tkinter import *
 from tkinter import ttk
+
 root = Tk()
-frm = ttk.Frame(root, padding=10)
+
+root.geometry("300x200") 
+
+frm = ttk.Frame(root, padding=50)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0, pady=10)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1, pady=10)
+
+# Center everything inside the window
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+frm.columnconfigure(0, weight=1)
+frm.rowconfigure(0, weight=1)
+
 root.mainloop()
