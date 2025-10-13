b23fbd8c0c2fd84be1dd4804e700bc0afa7f9cec
g-pyda <gabriela.pyda4@gmail.com>
2025-10-13 14:07:20 +0200

    changed proj name

--- src/hello_world_project/main.py
+++ src/hello_world_project/main.py
@@ -2,7 +2,7 @@ from hello_world_project.utils import greet_user
 from hello_world_project.example_module import add_numbers
 
 def main():
-    print("=== Hello World Project ===")
+    print("=== Planets movement project ===")
 
     name = input("Enter your name: ")
     greeting = greet_user(name)
