71d6072bdc9b3de7ac756544ca2b82910abbb439
AdamBalski <adambalski1@gmail.com>
2025-10-13 14:01:17 +0200

    bootstrap logic

--- src/tic_tac_toe/main.py
+++ src/tic_tac_toe/main.py
@@ -1,4 +1,4 @@
-from src.tic_tac_toe.utils import ask_for_name
+from src.tic_tac_toe.utils import GameState, ask_for_name, turn
 
 def main():
     print("=== Tic Tac Toe ===")
@@ -6,6 +6,12 @@ def main():
     name1 = ask_for_name("player #1")
     name2 = ask_for_name("player #2")
     print(f"Starting game for {name1} and {name2}")
+
+    state = None #todo: initialize state
+    p1_turn = True
+    while state.state() == GameState.UNFINISHED:
+        turn(state, p1_turn)
+        p1_turn = not p1_turn
 
 if __name__ == "__main__":
     main()
