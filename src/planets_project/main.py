c6b477357945b618e9c7cf1c416b6b6003d9c829
g-pyda <gabriela.pyda4@gmail.com>
2025-10-13 14:42:24 +0200

    project layout

--- src/planets_project/main.py
+++ src/planets_project/main.py
@@ -4,12 +4,5 @@ from hello_world_project.example_module import add_numbers
 def main():
     print("=== Planets movement project ===")
 
-    name = input("Enter your name: ")
-    greeting = greet_user(name)
-    print(greeting)
-
-    result = add_numbers(10, 5)
-    print(f"The result of adding 10 and 5 is {result}.")
-
 if __name__ == "__main__":
     main()
