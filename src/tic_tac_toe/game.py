e147d0cac1532a9d79c2dd56595a5b634a84bf16
Alicja <alicja@linux-ala>
2025-10-13 14:43:16 +0200

    Beginning of sketch of game logic (sketch of endgame function that checks if there is a draw or win after each move)

---
+++ src/tic_tac_toe/game.py
@@ -0,0 +1,18 @@
+class Gameplay:
+    def __init__(self):
+        def winCheck:
+
+        def endgame(board):
+            win = True
+            draw = True
+            # check if board full
+            for i in board:
+                for j in board[i]:
+                    if board[i][j] != " ":
+                        count += 1
+            if count = 9:
+                #check if win
+
+            # check rows
+            # check columns
+            # check diagonal
