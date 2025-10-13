4f9953abd79aaa437a0ffc7f22e9fa7e449526a7
Kamil Krawiec <KamilKrawiec@student.agh.edu.pl>
2025-10-13 14:30:17 +0200

    Added print_state feature

--- src/tic_tac_toe/utils.py
+++ src/tic_tac_toe/utils.py
@@ -5,7 +5,7 @@ import typing
 def ask_for_name(player_tag) -> str:
     return input(f"Enter name of {player_tag}:")
 
-class GameState(enum.EnumType):
+class GameState(enum.Enum):
     PLAYER_1 = 1
     PLAYER_2 = 2
     DRAW = 3
@@ -21,8 +21,18 @@ class State:
         return GameState.UNFINISHED
 
 def print_state(state: State):
-    # todo
-    pass
+    match state.state():
+        case 1:
+            print("X won")
+        case 2:
+            print("O won")
+        case 3:
+            print("Draw")
+        case 4:
+            print("Unfinished")
+    msg = "  a b c" + "\n".join(str(index+1) + " " + " ".join(row) for index,row in enumerate(state.board))
+    print(msg
+          )
 def turn(state: State, p1_turn: bool):
     # todo
     pass
