cd86d278ceeeba05a583150499831d3d2cb989f6
artur_zamorowski <arturzamorowskiiiiii@gmail.com>
2025-10-17 22:04:59 +0200

    minor changes

---
+++ src/magball_project/ball.py
@@ -0,0 +1,12 @@
+class ball:
+    def __init__(self, radius, x, y, xVel, yVel, charge, color, weight):
+        self.radius = radius
+        self.x = x
+        self.y = y
+        self.xVel = xVel
+        self.yVel = yVel
+        self.charge = charge
+        self.color = color
+        self.weight = weight
+
+    #TODO engine
