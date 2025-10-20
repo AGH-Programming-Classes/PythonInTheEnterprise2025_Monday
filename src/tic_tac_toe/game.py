de1d471e10f26ce22731e5f5395f269202e8af7c
Jakub Jucha <kuba.jucha.24@gmail.com>
2025-10-20 15:31:40 +0200

    bugfixes

--- src/tic_tac_toe/game.py
+++ src/tic_tac_toe/game.py
@@ -4,43 +4,43 @@ class Gameplay:
     def __init__(self):
         self.board = Board()
         self.activeplayer = "X"
+
     def boardStatus(self):
+        board_state = self.board.getBoardState()
         count = 0
-        for i in self.board.size:
-            for j in self.board[i]:
-                if self.board[i][j] != " ":
+        for i in range(self.board.size):
+            for j in range(self.board.size):
+                if board_state[i][j] != " ":
                     count += 1
         if count == 9:
             return "full"
 
     def winCheck(self):
         win = False
-        for i in self.board.size:
+        board_state = self.board.getBoardState()
+        for i in range(self.board.size):
         # check rows
-            if self.board[i][0] == self.board[i][1] == self.board[i][2]:
-                if self.board[i][0] != " ":
+            if board_state[i][0] == board_state[i][1] == board_state[i][2]:
+                if board_state[i][0] != " ":
                     win = True
         # check columns
-            if self.board[0][i] == self.board[1][i] == self.board[2][i]:
-                if self.board[0][i] != " ":
+            if board_state[0][i] == board_state[1][i] == board_state[2][i]:
+                if board_state[0][i] != " ":
                     win = True
         # check diagonal
-        if (self.board[0][0] == self.board[1][1] == self.board[2][2]) or (self.board[0][2] == self.board[1][1] == self.board[2][0]):
-            if self.board[0][0] != " ":
+        if (board_state[0][0] == board_state[1][1] == board_state[2][2]) or (board_state[0][2] == board_state[1][1] == board_state[2][0]):
+            if board_state[1][1] != " ":
                 win = True
         return win
 
     def endgame(self):
-        win = False
         draw = False
+        win = self.winCheck()
 
-        if self.boardStatus(self.board) == "full":
-            if self.winCheck(self.board) == False:
-                draw = True
-            else:
-                win = True
+        if self.boardStatus() == "full":
+            draw = True
 
-        if win==True:
+        if win == True:
             return "WIN"
         if draw == True:
             return "DRAW"
@@ -49,18 +49,21 @@ class Gameplay:
 
     def start(self):
         while True:
-            self.board.draw_board_idx()
-            p=int(input(f"Player {self.activeplayer} enters"))
+            self.board.draw()
+            p=int(input(f"Player {self.activeplayer} enters: "))
             # chech if move legal
             self.board.setCell(p, self.activeplayer)
-            if self.endgame() == "Continue":
+            end = self.endgame()
+            if end == "Continue":
                 self.activeplayer = "O" if self.activeplayer == "X" else "X"
             else:
+                self.board.draw()
+                print("Game Over")
+                if end == "DRAW":
+                    print("Draw")
+                if end == "WIN":
+                    print(f"Winner is {self.activeplayer}")
                 break
-        print("Game Over")
-        if self.endgame() == "DRAW":
-            print("Draw")
-        if self.endgame() == "WIN":
-            print(f"Winner is {self.activeplayer}")
+
 
 
