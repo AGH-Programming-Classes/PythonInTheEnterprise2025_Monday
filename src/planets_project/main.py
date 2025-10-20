71d1bd9c44cc0b5927aaf522d628a2b2dbeb90c3
g-pyda <gabriela.pyda4@gmail.com>
2025-10-20 14:11:02 +0200

    added project layout

--- src/planets_project/main.py
+++
@@ -1,8 +0,0 @@
-from hello_world_project.utils import greet_user
-from hello_world_project.example_module import add_numbers
-
-def main():
-    print("=== Planets movement project ===")
-
-if __name__ == "__main__":
-    main()
