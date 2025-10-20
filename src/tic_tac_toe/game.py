d033d5a213dfd94463bf603a6c62b867d4bf4a71
Alicja <alicja@linux-ala>
2025-10-20 14:02:45 +0200

    preparing the win logic

--- src/tic_tac_toe/game.py
+++ src/tic_tac_toe/game.py
@@ -1,22 +1,30 @@
+from tic_tac_toe.board import Board
+
 class Gameplay:
     def __init__(self):
-        def winCheck:
-
-        def endgame(board):
-            win = TrueBe
-            draw = True
-            # check if board full
+        def boardStatus(Board):
+            count = 0
             for i in board:
                 for j in board[i]:
                     if board[i][j] != " ":
                         count += 1
-            if count = 9:
-                #check if win
+            if count == 9:
+                return "full"
 
-            for i in board:
+        def winCheck(Board):
+            for i in Board:
                 if board[i][0] == board[i][1] == board[i][2]:
                     if board[i][0] != " ":
                         win = True
+
             # check rows
             # check columns
             # check diagonal
+
+        def endgame(Board):
+            win = False
+            draw = False
+
+            if boardStatus(Board) == "full":
+                if winCheck(Board) == False:
+                    draw = True
