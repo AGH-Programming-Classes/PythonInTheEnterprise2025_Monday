3cb1311cbe6c661e7633065aec9738c29ae3b0d0
Alicja <alicja@linux-ala>
2025-10-20 14:27:10 +0200

    cleaning code, including Board

--- src/tic_tac_toe/game.py
+++ src/tic_tac_toe/game.py
@@ -2,29 +2,37 @@ from tic_tac_toe.board import Board
 
 class Gameplay:
     def __init__(self):
-        def boardStatus(Board):
-            count = 0
-            for i in board:
-                for j in board[i]:
-                    if board[i][j] != " ":
-                        count += 1
-            if count == 9:
-                return "full"
+        self.board = Board()
+    def boardStatus(self):
+        count = 0
+        for i in self.board.size:
+            for j in self.board[i]:
+                if self.board[i][j] != " ":
+                    count += 1
+        if count == 9:
+            return "full"
 
-        def winCheck(Board):
-            for i in Board:
-                if board[i][0] == board[i][1] == board[i][2]:
-                    if board[i][0] != " ":
-                        win = True
+    def winCheck(self):
+        win = False
+        for i in self.board.size:
+        # check rows
+            if self.board[i][0] == self.board[i][1] == self.board[i][2]:
+                if self.board[i][0] != " ":
+                    win = True
+        # check columns
+            if self.board[0][i] == self.board[1][i] == self.board[2][i]:
+                if self.board[0][i] != " ":
+                    win = True
+        # check diagonal
+        if (self.board[0][0] == self.board[1][1] == self.board[2][2]) or (self.board[0][2] == self.board[1][1] == self.board[2][0]):
+            if self.board[0][0] != " ":
+                win = True
+        return win
 
-            # check rows
-            # check columns
-            # check diagonal
+    def endgame(self):
+        win = False
+        draw = False
 
-        def endgame(Board):
-            win = False
-            draw = False
-
-            if boardStatus(Board) == "full":
-                if winCheck(Board) == False:
-                    draw = True
+        if self.boardStatus(self.board) == "full":
+            if self.winCheck(self.board) == False:
+                draw = True
