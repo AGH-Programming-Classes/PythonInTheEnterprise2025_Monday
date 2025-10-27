813eb977100401eb3a9de288587c58e5791a1e5a
Alicja <alicja@linux-ala>
2025-10-27 14:10:37 +0100

    Utils file and minor changes in gameplay logic

--- src/tic_tac_toe/game.py
+++ src/tic_tac_toe/game.py
@@ -1,4 +1,5 @@
 from tic_tac_toe.board import Board
+from GameUtils import Utils
 
 class Gameplay:
     def __init__(self, player1, player2):
@@ -6,47 +7,6 @@ class Gameplay:
         self.players = [player1, player2]
         self.activeplayer = 0
 
-    def boardStatus(self):
-        board_state = self.board.getBoardState()
-        count = 0
-        for i in range(self.board.size):
-            for j in range(self.board.size):
-                if board_state[i][j] != " ":
-                    count += 1
-        if count == 9:
-            return "full"
-
-    def winCheck(self):
-        win = False
-        board_state = self.board.getBoardState()
-        for i in range(self.board.size):
-        # check rows
-            if board_state[i][0] == board_state[i][1] == board_state[i][2]:
-                if board_state[i][0] != " ":
-                    win = True
-        # check columns
-            if board_state[0][i] == board_state[1][i] == board_state[2][i]:
-                if board_state[0][i] != " ":
-                    win = True
-        # check diagonal
-        if (board_state[0][0] == board_state[1][1] == board_state[2][2]) or (board_state[0][2] == board_state[1][1] == board_state[2][0]):
-            if board_state[1][1] != " ":
-                win = True
-        return win
-
-    def endgame(self):
-        draw = False
-        win = self.winCheck()
-
-        if self.boardStatus() == "full":
-            draw = True
-
-        if win == True:
-            return "WIN"
-        if draw == True:
-            return "DRAW"
-        else:
-            return "Continue"
     def nextPlayer(self):
         self.activeplayer = self.activeplayer ^ 1
 
@@ -54,6 +14,8 @@ class Gameplay:
         board_state = self.board.getBoardState()
         i = (move - 1) // self.board.size
         j = (move - 1) % self.board.size
+        if move == "":
+            return False
         if move < 1 or move > 9:
             return False
         if board_state[i][j] != " ":
